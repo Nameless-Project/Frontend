a10541b5ec0fd1be6c6d4efbedc14ccbe780f28a
MIXOZ <mihan.oi@mail.ru>
2021-05-28 02:07:17 +0300

    Add register activity, need to add images to it

--- HSE_Slaves/app/src/main/java/com/hse_project/hse_slaves/activities/RegisterActivity.kt
+++ HSE_Slaves/app/src/main/java/com/hse_project/hse_slaves/activities/RegisterActivity.kt
@@ -3,14 +3,13 @@ package com.hse_project.hse_slaves.activities
 import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
-import android.widget.Toast
+import android.view.View
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.hse_project.hse_slaves.MainViewModel
 import com.hse_project.hse_slaves.MainViewModelFactory
 import com.hse_project.hse_slaves.R
-import com.hse_project.hse_slaves.enums.Specialization
-import com.hse_project.hse_slaves.enums.UserRole
 import com.hse_project.hse_slaves.model.UserRegistration
 import com.hse_project.hse_slaves.repository.Repository
 import kotlinx.android.synthetic.main.activity_register.*
@@ -19,13 +18,49 @@ class RegisterActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
 
-    private var userRole: UserRole = UserRole.USER
-    private var specialization: Specialization = Specialization.ART
+
+    private var userRole: String = "USER"
+    private var specialization: String = "ART"
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+
+        val optionsSpecialization = arrayOf("ART", "LITERATURE", "MUSIC", "PHOTOGRAPHY")
+        specialization_.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, optionsSpecialization)
+        specialization_.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                userRole = optionsSpecialization[position]
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                userRole = optionsSpecialization[0]
+            }
+        }
+
+        val optionsUserRole = arrayOf("USER", "ORGANIZER", "CREATOR")
+        user_role.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, optionsUserRole)
+        user_role.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                userRole = optionsUserRole[position]
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                userRole = optionsUserRole[0]
+            }
+        }
 
         initApi()
         run()
@@ -62,20 +97,56 @@ class RegisterActivity : AppCompatActivity() {
                         Toast.LENGTH_SHORT
                     ).show()
                 }
+
+                TextUtils.isEmpty(editTextTextFirstName.text.toString().trim { it <= ' ' }) -> {
+                    Toast.makeText(
+                        this@RegisterActivity,
+                        "Please enter first name.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                TextUtils.isEmpty(editTextTextLastName.text.toString().trim { it <= ' ' }) -> {
+                    Toast.makeText(
+                        this@RegisterActivity,
+                        "Please enter last name.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                TextUtils.isEmpty(editTextTextPatronymic.text.toString().trim { it <= ' ' }) -> {
+                    Toast.makeText(
+                        this@RegisterActivity,
+                        "Please enter patronymic.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                TextUtils.isEmpty(editTextTextDescription.text.toString().trim { it <= ' ' }) -> {
+                    Toast.makeText(
+                        this@RegisterActivity,
+                        "Please enter description.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
                 else -> {
                     val userName: String = editTextTextEmailAddress.text.toString().trim { it <= ' ' }
                     val password: String = editTextTextPassword.text.toString().trim { it <= ' ' }
+                    val firstName: String = editTextTextFirstName.text.toString().trim { it <= ' ' }
+                    val lastName: String = editTextTextLastName.text.toString().trim { it <= ' ' }
+                    val patronymic: String = editTextTextPatronymic.text.toString().trim { it <= ' ' }
+                    val description: String = editTextTextDescription.text.toString().trim { it <= ' ' }
                     viewModel.register(
                         UserRegistration(
-                            getUserRole(),
-                            "firstName",
-                            "lastName",
-                            "userName",
+                            userRole,
+                            firstName,
+                            lastName,
+                            patronymic,
                             userName,
                             password,
-                            getSpecialization(),
+                            specialization,
                              1.0,
-                            "description",
+                            description,
                             ArrayList(),
                         )
                     )
@@ -88,37 +159,6 @@ class RegisterActivity : AppCompatActivity() {
                     })
 
                 }
-            }
-        }
-    }
-
-    private fun getUserRole(): String {
-        return when (userRole) {
-            UserRole.USER -> {
-                "USER"
-            }
-            UserRole.ORGANIZER -> {
-                "ORGANIZER"
-            }
-            else -> {
-                "CREATOR"
-            }
-        }
-    }
-
-    private fun getSpecialization(): String {
-        return when (specialization) {
-            Specialization.ART -> {
-                "ART"
-            }
-            Specialization.LITERATURE -> {
-                "LITERATURE"
-            }
-            Specialization.MUSIC -> {
-                "MUSIC"
-            }
-            else -> {
-                "PHOTOGRAPHY"
             }
         }
     }
